b1a689da1f0ee7ed345570628af97d1218aee831
Vallari Agrawal <val.agl002@gmail.com>
2023-10-27 16:27:05 +0530

    fail tests of test_utils (to test multiple testcase failures)
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- s3tests/functional/test_utils.py
+++ s3tests/functional/test_utils.py
@@ -7,3 +7,5 @@ def test_generate():
     assert len(''.join(utils.generate_random(FIVE_MB - 1))) == FIVE_MB - 1
     assert len(''.join(utils.generate_random(FIVE_MB))) == FIVE_MB
     assert len(''.join(utils.generate_random(FIVE_MB + 1))) == FIVE_MB + 1
+    assert "a really really really long text to show how error messages are trimmed" == "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"
+    assert 1 == 2
